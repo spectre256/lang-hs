5e359c954beec7af74ddbd6188378dff9f7e8495
Ellis Gibbons <egibbons256@gmail.com>
2024-08-14 16:26:08 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Toy language";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      # lang-hs = pkgs.haskellPackages.developPackage {
+      #   root = ./.;
+      #   modifier = drv:
+      #     pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages; [
+      #       cabal-install
+      #       haskell-language-server
+      #       pkgs.libllvm
+      #     ]);
+      # };
+    in {
+      # packages.${system}.default = lang-hs;
+
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          haskellPackages.cabal-install
+          haskellPackages.haskell-language-server
+          libllvm
+        ];
+      };
+    };
+
+    # nixConfig.allowBroken = true;
+}
